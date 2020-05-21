6ce0b44bdad9359b1facfe133d6ba882262bbfe6
Ronnie A. Baez Sesto <ronnie.baez@gmail.com>
2020-05-21 21:21:03 +0200

    adds func that creates a new tagshelf client
    based on credentials provided

--- tagshelf/tagshelf.go
+++ tagshelf/tagshelf.go
@@ -2,7 +2,29 @@ package tagshelf
 
 import "fmt"
 
-// New creates a configuration free instance of the client
-func New() {
-	fmt.Printf("Tagshelf API Go client")
+// New creates new client
+func New(config Config) (c Requester, err error) {
+	if config.AppApiKey != "" {
+		client := &AppApiKeyClient{}
+		c, err = client.Auth(config)
+	}
+
+	if config.ApiKey != "" && config.SecretKey != "" {
+		client := &HMACClient{}
+		c, err = client.Auth(config)
+	}
+
+	if config.GrantType != "" && config.User != "" && config.Pass != "" {
+		client := &OAuthClient{}
+		c, err = client.Auth(config)
+	}
+
+	if c != nil {
+		return
+	}
+
+	if err == nil {
+		err = fmt.Errorf("No credentials provided")
+	}
+	return
 }
